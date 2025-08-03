778b7ad8e3dd54650057b69cc627fc702bf0dc3a
Your Name <your.email@example.com>
2025-08-02 20:21:57 -0700

    new files

---
+++ internal/core/entity/patient.go
@@ -0,0 +1,11 @@
+package entity
+
+type Patient struct {
+	ID        string `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Phone     string `json:"phone"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+}
---
+++ internal/core/port/patient_service.go
@@ -0,0 +1,11 @@
+package port
+
+import "github.com/jairo/patient-api/internal/core/entity"
+
+type PatientService interface {
+	CreatePatient(*entity.Patient) error
+	GetPatientByID(string) (*entity.Patient, error)
+	UpdatePatient(*entity.Patient) error
+	DeletePatient(string) error
+	ListPatients() ([]*entity.Patient, error)
+}
